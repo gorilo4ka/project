348f0cf495ff15511cc9ab3a8b3460642c2d291c
Myrcats <Squirrell97@gmail.com>
2017-03-07 21:34:54 +0200

    ++

--- list/list/Program.cs
+++ list/list/Program.cs
@@ -13,25 +13,39 @@ namespace list
             Random rnd = new Random();
             List<int> list = new List<int>();
             List<int> list_even = new List<int>();
-
-            for (int i = 0; i < 51; i++)
+            int count=0;
+            int position = -1;
+            for (int i = 0; i < 20; i++)
             {
-                if (i == 0)
-                {
-                    list.Add(rnd.Next(0, 16));
-                }
                 list.Add(rnd.Next(0, 100));
             }
-
             foreach (var item in list)
             {
-                if (item % 2 == 0)
-                    list_even.Add(item);
+                if (item > 50)
+                    count++;                   
+                if (item % 2 == 1)
+                list_even.Add(item);                
+            }
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i]<16)
+                {
+                    position = i;
+                    break;
+                }
             }
             foreach (int items in list_even)
             {
                 Console.WriteLine(items);
             }
+            Console.WriteLine();
+            foreach (int items in list)
+            {
+                Console.WriteLine(items);
+            }
+            Console.WriteLine();
+            Console.WriteLine("Количество >50 - "+count);
+            Console.WriteLine("позиция <16 - "+position);
             foreach (int items in list_even)
             {
                 if (items > 90)
@@ -39,6 +53,7 @@ namespace list
                     Console.WriteLine("есть больше 90");
                     break;
                 }
+
             }
             Console.ReadKey();
         }
